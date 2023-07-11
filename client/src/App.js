64caa22831db2aa45c8615edb51eb88782ad9578
abhishekwazal <abhishekwazal07@gmail.com>
2023-07-11 12:47:28 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Context } from "./context/Context";
+
+
+function App() {
+  const {user}=useContext(Context);
+  return (
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/register" element={user ? <Homepage /> : <Register />} />
+        <Route path="/login" element={user ? <Homepage /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
